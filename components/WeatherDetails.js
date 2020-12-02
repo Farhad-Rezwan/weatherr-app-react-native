5dd1d896b5bbd052c8223f44b9b9fd1a6dd963d0
Farhad Ullah Rezwan <53939630+Farhad-Rezwan@users.noreply.github.com>
2020-12-03 08:50:01 +1100

    Finishing up with development

--- components/WeatherDetails.js
+++ components/WeatherDetails.js
@@ -5,10 +5,15 @@ import {FontAwesome5, MaterialCommunityIcons} from "@expo/vector-icons";
 
 const {PRIMARY_COLOR, SECONDARY_COLOR, BORDER_COLOR} = colors;
 
-export default function WeatherDetails({currentWeather}) {
+export default function WeatherDetails({currentWeather, unitsSystem}) {
     const {
         main: {feels_like, humidity, pressure},
+        wind: {speed}
     } = currentWeather;
+
+    const windSpeed = unitsSystem === 'metric' ? `${Math.round(speed)} m/s` : `${Math.round(speed)} m/h`
+
+
     return (
         <View style={styles.weatherDetails}>
             <View style={styles.weatherDetailsRow}>
@@ -27,9 +32,9 @@ export default function WeatherDetails({currentWeather}) {
                             color={PRIMARY_COLOR}
                         />
                         <View style={styles.weatherDetailsItems}>
-                            <Text> feels like:</Text>
+                            <Text> Feels like:</Text>
                             <Text style={styles.textSecondary}>
-                                {feels_like}
+                                {feels_like}°
                             </Text>
                         </View>
                     </View>
@@ -49,7 +54,50 @@ export default function WeatherDetails({currentWeather}) {
                         />
                         <View style={styles.weatherDetailsItems}>
                             <Text> Humidity:</Text>
-                            <Text style={styles.textSecondary}>{humidity}</Text>
+                            <Text style={styles.textSecondary}>{humidity}% </Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+            <View style={{...styles.weatherDetailsRow, borderTopWidth: 1, borderColor: BORDER_COLOR}}>
+                {/* to deviede the cell on right */}
+                <View
+                    style={{
+                        ...styles.weatherDetailsBox,
+                        borderRightWidth: 1,
+                        borderRightColor: BORDER_COLOR,
+                    }}
+                >
+                    <View style={styles.weatherDetailsRow}>
+                        <MaterialCommunityIcons
+                            name="weather-windy"
+                            size={25}
+                            color={PRIMARY_COLOR}
+                        />
+                        <View style={styles.weatherDetailsItems}>
+                            <Text> Wind Speed:</Text>
+                            <Text style={styles.textSecondary}>
+                                {windSpeed}
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+                <View
+                    style={{
+                        ...styles.weatherDetailsBox,
+                        borderRightWidth: 1,
+                        borderRightColor: BORDER_COLOR,
+                    }}
+                >
+                    <View style={styles.weatherDetailsRow}>
+                        <MaterialCommunityIcons
+                            name="speedometer"
+                            size={30}
+                            color={PRIMARY_COLOR}
+                        />
+                        <View style={styles.weatherDetailsItems}>
+                            <Text> Pressure:</Text>
+                            <Text style={styles.textSecondary}>{pressure} hPa</Text>
                         </View>
                     </View>
                 </View>
